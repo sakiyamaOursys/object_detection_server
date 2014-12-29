9ec3f736a56557027c4b1e8fb430b7fb7af3ad42
sakiyamaOursys <sakiyama@oursys.co.jp>
2014-12-30 01:57:26 +0900

    ファイル修正

--- obj_detection.php
+++ obj_detection.php
@@ -1,7 +1,7 @@
 <?php
-    $fileNameFullPath = $_GET["fileName"]; //"000_lena.jpg";
+    $fileNameFullPath = "test_img/" . $_GET["fileName"]; //"000_lena.jpg";
     $cascadeFileNameFullPath = "haarcascades/haarcascade_cat.xml";
-    $exe = "./build/xml_detection " . $fileNameFullPath . " " . $cascadeFileNameFullPath;
+    $exe = "./build/obj_detection " . $fileNameFullPath . " " . $cascadeFileNameFullPath;
 
     echo $exe . "</br>\n";
     system($exe, $ret);
